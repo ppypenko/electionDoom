cd32ce997c9571e837efffb489a67f407dc7a1f7
pyotr pypenko <pyotrspypenko@gmail.com>
2016-02-14 11:54:50 -0700

    Finished poll hierarchy.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 $.ajax({
     url: 'http://elections.huffingtonpost.com/pollster/api/polls.json?callback=pollsterPoll&page=3&state=US&topic=2016-president',
     dataType: 'script',
@@ -6,47 +7,38 @@ $.ajax({
 });
 
 window.pollsterPoll = function (incoming_data) {
-    console.log(incoming_data);
     var html = "",
         block,
-        ind = 0,
-        dem = 0,
-        rep = 0;
-    for (var i = 0; i < 3; i++) {
+        i = 0,
+        j = 0,
+        k = 0;
+    for (i = 0; i < incoming_data.length; i += 1) {
         block = document.createElement("section");
         block.setAttribute("id", "tab-" + i);
-        if (incoming_data[i].affiliation === "None") {
-            ind += 1;
-            block.setAttribute("class", "ind");
-            html += "<h2>Affiliation: Independant</h2>";
-            document.getElementById("tablinks").innerHTML += "<li><a href='#tab-" + i + "'>Independant" + ind + "</a></li>";
-        } else if (incoming_data[i].affiliation === "Dem") {
-            dem += 1;
-            block.setAttribute("class", "dem");
-            html += "<h2>Affiliation: Democrat</h2>";
-            document.getElementById("tablinks").innerHTML += "<li><a href='#tab-" + i + "'>Democrat" + dem + "</a></li>";
-        } else if (incoming_data[i].affiliation === "Rep") {
-            rep += 1;
-            block.setAttribute("class", "rep");
-            html += "<h2>Affiliation: Republican</h2>";
-            document.getElementById("tablinks").innerHTML += "<li><a href='#tab-" + i + "'>Republican" + rep + "</a></li>";
-        }
-        block.innerHTML = html;
-        //console.log(incoming_data[i].affiliation);
-        //console.log(incoming_data[i].pollster);
-        //console.log(incoming_data[i].method);
+        block.setAttribute("class", incoming_data[i].affiliation);
+        html += "<h2>Pollster: " + incoming_data[i].pollster + "</h2>" +
+            "<h2>Affiliation: " + incoming_data[i].affiliation + "</h2>" +
+            "<h2>Type of Poll: " + incoming_data[i].method + "</h2>";
+        document.getElementById("tablinks").innerHTML += "<li><a href='#tab-" + i + "'>" + incoming_data[i].pollster + "</a></li>";
+
         //console.log(incoming_data[i].partisan);
-        //        for (var j = 0; j < 3; j++) {
-            console.log(incoming_data[i].questions[j].name);
-        //            for (var k = 0; k < incoming_data[i].questions[j].subpopulations[0].responses.length; k++) {
-        //                //console.log(incoming_data[i].questions[j].subpopulations[0].responses[k].choice);
-        //                //console.log(incoming_data[i].questions[j].subpopulations[0].responses[k].value);
-        //            }
-        //        }
+        for (j = 0; j < incoming_data[i].questions.length; j += 1) {
+            html += "<section class='question'>" +
+                "<h3>" + incoming_data[i].questions[j].name + "</h3>";
+            for (k = 0; k < incoming_data[i].questions[j].subpopulations[0].responses.length; k += 1) {
+                html += "<section class='reply'>" +
+                    "<h4>" + incoming_data[i].questions[j].subpopulations[0].responses[k].choice + "</h4>" +
+                    "<h6>" + incoming_data[i].questions[j].subpopulations[0].responses[k].value + "</h6>" + "</section>";
+            }
+            html += "</section>";
+            block.innerHTML = html;
+        }
         document.getElementById("tabs").appendChild(block);
         html = "";
     }
 };
-$(function () {
-    $("#tabs").tabs();
-});
+window.onload = function () {
+    $(function () {
+        $("#tabs").tabs();
+    });
+};
